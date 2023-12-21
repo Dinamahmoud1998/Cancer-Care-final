dbc7625bed421ff3278239f54603c2005f034012
Dinamahmoud1998 <154019698+Dinamahmoud1998@users.noreply.github.com>
2023-12-21 09:58:33 +0200

    Add files via upload

---
+++ Final project/final project.js
@@ -0,0 +1,23 @@
+function visible(){
+document.getElementById("line").style.display='block'
+
+
+
+}
+
+function non(){
+    document.getElementById("line").style.display='none'
+    
+
+    }
+    
+    function eraseText() {
+        document.getElementById("w3review").value = "";
+        
+    }
+
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+    
+        div.doc.add('a-flip-right');
+    })
